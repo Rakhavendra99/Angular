8fd6b6b971b5a3b02a3402a5a53d21e51f70c9c8
Rakhavendra <rakhavendra631999@gmail.com>
2021-04-27 20:07:49 +0530

    Udemy account is working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,32 +23,32 @@ export class AppComponent {
     company:'proglint',
     salary:8000.98
   }
-  // selectedUser = '';
-  // showUser: boolean = true;
+  selectedUser = '';
+  showUser: boolean = true;
 
-  // users = [
-  //   { name: 'ram', employee_id: '12', age: 31 },
-  //   { name: 'mohan', employee_id: '13', age: 30 },
-  //   { name: 'abi', employee_id: '14', age: 32 },
-  //   { name: 'veena', employee_id: '15', age: 29 },
-  //   { name: 'raj', employee_id: '16', age: 35 },
-  // ];
-  // addMember(){
-  //   this.users = [
-  //     { name: 'ram', employee_id: '12', age: 31 },
-  //     { name: 'mohan', employee_id: '13', age: 30 },
-  //     { name: 'abi', employee_id: '14', age: 32 },
-  //     { name: 'veena', employee_id: '15', age: 29 },
-  //     { name: 'raj', employee_id: '16', age: 35 },
-  //     { name: 'ravi', employee_id: '10', age: 22 },
-  //     { name: 'pravin', employee_id: '7', age: 5 },
-  //   ];
-  // }
+  users = [
+    { name: 'ram', employee_id: '12', age: 31 },
+    { name: 'mohan', employee_id: '13', age: 30 },
+    { name: 'abi', employee_id: '14', age: 32 },
+    { name: 'veena', employee_id: '15', age: 29 },
+    { name: 'raj', employee_id: '16', age: 35 },
+  ];
+  addMember(){
+    this.users = [
+      { name: 'ram', employee_id: '12', age: 31 },
+      { name: 'mohan', employee_id: '13', age: 30 },
+      { name: 'abi', employee_id: '14', age: 32 },
+      { name: 'veena', employee_id: '15', age: 29 },
+      { name: 'raj', employee_id: '16', age: 35 },
+      { name: 'ravi', employee_id: '10', age: 22 },
+      { name: 'pravin', employee_id: '7', age: 5 },
+    ];
+  }
 
-  // // ngIf
-  // showElements:boolean=true;
+  // ngIf
+  showElements:boolean=true;
 
-  // number:String='1';
+  number:String='1';
 
   datas:Array<any>
   headers=["ID","Title","Body"]
@@ -68,47 +68,44 @@ export class AppComponent {
     this.isTrue = !this.isTrue;
   }
 
-  // constructor(private JSONPlaceholder:JSONPlaceholderService){
-    
-  // }
 
 
   // Post = {
   //   title:"Title",
   //   isFavorite:true
   // }
-  // largeFont="large-font green"
-  // smallFont="small-font"
-  // bluecolor="blue"
+  largeFont="large-font green"
+  smallFont="small-font"
+  bluecolor="blue"
 
-  // date=new Date()
-  // myName="rakhavendra"
-  // myNumber=12345.69870
-  // myCurrency=45000.56
+  date=new Date()
+  myName="rakhavendra"
+  myNumber=12345.69870
+  myCurrency=45000.56
 
-  // animals =[
-  //   'Lion',
-  //   'tiger',
-  //   'fox',
-  //   'cheeta',
-  //   'dog'
-  // ]
+  animals =[
+    'Lion',
+    'tiger',
+    'fox',
+    'cheeta',
+    'dog'
+  ]
 
-  // helpPopup = false;
-  // feedback = '';
-  // kioskId = '';
-  // checkoutId = '';
+  helpPopup = false;
+  feedback = '';
+  kioskId = '';
+  checkoutId = '';
 
   // constructor(){
   //     } 
 
-  //   showHelpPopup() {
-  //       this.helpPopup = true;
-  //   }
+    showHelpPopup() {
+        this.helpPopup = true;
+    }
 
-  //   closePopup() {
-  //       this.helpPopup = false;
-  //   }
+    closePopup() {
+        this.helpPopup = false;
+    }
 
 
 
--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -14,6 +14,22 @@ export class FavoriteComponent implements OnInit  {
   list=[1,5,2,4,3];
   constructor(){    
   }
+  viewMode = "otherlist view";
+
+  courses=[
+    {id:1 , name:"course - 1" },
+    {id:2 , name:"course - 2" },
+    {id:3 , name:"course - 3" },
+    {id:4 , name:"course - 4" }
+  ]
+
+  onAdd(){
+    this.courses.push({id:5,name:'course - 5'});
+  }
+  onRemove(i:any){
+    let index = this.courses.indexOf(i);
+    this.courses.splice(index,1);
+  }
 
   ngOnInit(){
   }
